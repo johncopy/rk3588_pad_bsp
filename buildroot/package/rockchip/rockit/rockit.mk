4bbe27786dd536cc10b6c9be57889e63d3da3a7e
陈鹤群 <chen.hequn@ghtchina.com>
2026-05-06 17:30:03 +0800

    add buildroot

---
+++ buildroot/package/rockchip/rockit/rockit.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# rockit project
+#
+################################################################################
+
+ROCKIT_SITE = $(TOPDIR)/../external/rockit
+
+ROCKIT_SITE_METHOD = local
+
+ROCKIT_INSTALL_STAGING = YES
+
+ROCKIT_DEPENDENCIES = rockchip-mpp rockchip-rga alsa-lib
+
+ifneq ($(BR2_PACKAGE_RK3308),)
+ROCKIT_CONF_OPTS += -DRK3308=TRUE
+endif
+
+ifneq ($(BR2_PACKAGE_RK3506),)
+ROCKIT_CONF_OPTS += -DRK3506=TRUE
+endif
+
+$(eval $(cmake-package))
